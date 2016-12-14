cc0cc8bb7136eee710e1e71f7a27d7d84764de16
WebTrick <1511134675@qq.com>
2016-12-14 12:01:25 +0800

    Update type.js

--- type.js
+++ type.js
@@ -81,7 +81,7 @@ var Typer={
 	updLstChr:function(){ // blinking cursor
 		var cont=this.content(); // get console 
 		if(cont.substring(cont.length-1,cont.length)=="|") // if last char is the cursor
-			$("#console").html($("#console").html().substring(0,cont.length-1)); // remove it
+			document.getElementById('console').innerHTML = document.getElementById('console').innerHTML.substring(0,cont.length-1); // remove it
 		else
 			this.write("|"); // else write it
 	}
